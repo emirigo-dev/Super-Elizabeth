00bb2046c8ef2a4b0e52cbae2068a5f91624a260
Emiliano Rigobello <emirigobello@gmail>
2020-07-28 18:41:51 -0300

    Se eliminaron los sets de la clase Princesa

--- src/juego/Princesa.java
+++ src/juego/Princesa.java
@@ -24,41 +24,25 @@ public class Princesa {
 		return x;
 	}
 
-	public void setX(double x) {
-		this.x = x;
-	}
 
 	public double getY() {
 		return y;
 	}
 
-	public void setY(double y) {
-		this.y = y;
-	}
-
+	
 	public double getAltura() {
 		return altura;
 	}
 
-	public void setAltura(double altura) {
-		this.altura = altura;
-	}
 
 	public double getAncho() {
 		return ancho;
-	}
-
-	public void setAncho(double ancho) {
-		this.ancho = ancho;
 	}
 
 	public int getDireccion() {
 		return direccion;
 	}
 
-	public void setDireccion(int direccion) {
-		this.direccion = direccion;
-	}
 
 	public void dibujate () {
 		entorno.dibujarCirculo(this.x, this.y, 3, Color.GREEN);
